689f240852ee3260a3ebf9dd89db210414001727
Julien Bordellier <git@julienbordellier.com>
2014-07-23 11:04:24 +0200

    Update NZSocket.cc
    Line 83, you use the close function, defined in unistd.h.
    You need to include that file ;)

--- nzlib/src/NZSocket.cc
+++ nzlib/src/NZSocket.cc
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
